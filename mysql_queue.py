e0bc4c43144677cff31b48bbbddf7d2de5697575
roger lo <rogerlo@iii.org.tw>
2015-12-25 15:34:13 +0800

    完成enqueue,dequeue

--- mysql_queue.py
+++ mysql_queue.py
@@ -1,21 +1,46 @@
 import mysql.connector
-
+import time
 
 
 class mysql_queue:
-	def __init__(self,host,user,passwd,database):
+	def __init__(self,host,user,passwd,database,worker):
 		self.connect(host,user,passwd,database)
+		self.worker = worker
 
-	def connect():
-		self.cnx = mysql.connector(host,user,passwd,database)
-		selc.cur = cnx.cursor()
+	def connect(self,host,user,passwd,database):
+		self.cnx = mysql.connector.connect(host=host,user=user,passwd=passwd,database=database)
+		self.cur = self.cnx.cursor()
 
-	def enqueue(self,work_type,key,message,worker):
-		self.cur.execute('REPLACE INTO work_queue (type,key,message,worker,stat) VALUES (%s,%s,%s,%s,%s)',(work_type,key,message,worker,0))
+	def enqueue(self,work_type,key,request,worker):
+		self.cur.execute('REPLACE INTO works (`work_type`,`key`,`request`,`worker`,`stat`) VALUES (%s,%s,%s,%s,%s)',(work_type,key,request,worker,'pending'))
+		self.cnx.commit()
 		pass
 
-	def dequeue(work_type,key,worker):
-		pass
+	def dequeue(self,work_type,worker=None,key=None):
+		self.cur.execute('SET AUTOCOMMIT=0;')
+		self.cnx.commit()
+		sql = "SELECT `id`,`key`,`request` FROM works WHERE stat='pending' "
+		parameter = []
+		if work_type is not None:
+			sql += " AND `work_type`=%s"
+			parameter.append(work_type)
+		if worker is not None:
+			sql += " AND `worker`=%s"
+			parameter.append(worker)
+		if key is not None:
+			sql += " AND `key`=%s"
+			parameter.append(key)
+		sql += ' ORDER BY `id` LIMIT 1  FOR UPDATE'
+		self.cur.execute(sql,parameter)
+		result = self.cur.fetchone()
+		if result is None:
+			return None
+		work_key = result[1]
+		request = result[2]
+		self.cur.execute('UPDATE works SET `stat`=%s WHERE `key`=%s',(self.worker,work_key))
+		self.cnx.commit()
+		return {'work_key':work_key,'request':request}
+
 
 	def success(key,worker):
 		pass
